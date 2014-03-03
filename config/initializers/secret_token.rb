27eb733cbce2521a02d8febaf2fa6b78e7e275b2
Dmitry Zvorykin <webroom.pro@gmail.com>
2014-03-03 22:24:39 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webroom::Application.config.secret_key_base = '610f8d831eb5f26b95dcf439fd06bce396d3b55c61756f31c23be0f28869cb6d2f01d8496f54b5601cae1761358610d5c4cd937d20eaedc928be16af11c549b7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Webroom::Application.config.session_store :cookie_store, key: '_webroom_session'
